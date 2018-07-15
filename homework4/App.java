8ab497ca4b77c2d5fcb82cc680250373975aecec
AntonElenberger <antonelenberger@gmail.com>
2018-07-15 23:19:33 +0500

    1

--- homework4/App.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.antonelenberger.javacore1.homework4;
-
-public class App {
-    public static  void main(String[] args) {
-        ClientWindow mainWindow = new ClientWindow();
-        mainWindow.setVisible(true);
-    }
-}
